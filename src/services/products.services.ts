36d646ac261aeeac68dea6ba5452aa4dd8ef5a50
yasirarafatalif <yasirarafatalif1@gmail.com>
2026-05-14 22:57:02 +0600

    some

--- src/services/products.services.ts
+++ src/services/products.services.ts
@@ -5,7 +5,6 @@ import type { Product } from "../types/product.type";
 const filePath = path.join(process.cwd(), "./src/database/data.json");
 export const readProducts = () => {
   const products = fs.readFileSync(filePath, "utf-8");
-  // console.log("Products from data.json:", JSON.parse(products));
   return JSON.parse(products);
 };
 export const writeProducts = (data: Product) => {
